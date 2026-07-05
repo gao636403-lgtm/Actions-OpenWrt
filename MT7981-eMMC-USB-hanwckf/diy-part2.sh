923b0333e94e64f0907ea359d053b97247a226e4
gao636403-lgtm <gao636403@gmail.com>
2026-07-05 12:05:57 +0800

    Update diy-part2.sh

--- MT7981-eMMC-USB-hanwckf/diy-part2.sh
+++ MT7981-eMMC-USB-hanwckf/diy-part2.sh
@@ -25,3 +25,55 @@ rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns
 ##-----------------Manually set CPU frequency for MT7981B-----------------
 sed -i '/"mediatek"\/\*|\"mvebu"\/\*/{n; s/.*/\tcpu_freq="1.3GHz" ;;/}' package/emortal/autocore/files/generic/cpuinfo
+# ===== RAX3000M NAND custom first boot settings =====
+mkdir -p files/etc/uci-defaults
+
+cat > files/etc/uci-defaults/99-custom-settings <<'EOF'
+#!/bin/sh
+
+ROOT_PASSWORD='789998abcC@'
+
+WIFI_SSID='money99999999'
+WIFI_PASSWORD='13860775850'
+
+PPPOE_USERNAME='057993300017'
+PPPOE_PASSWORD='304975'
+
+# 设置 root 密码
+if [ -n "$ROOT_PASSWORD" ]; then
+  (echo "$ROOT_PASSWORD"; sleep 1; echo "$ROOT_PASSWORD") | passwd root
+fi
+
+# 设置 LAN IP
+uci set network.lan.ipaddr='192.168.3.1'
+uci set network.lan.netmask='255.255.255.0'
+uci commit network
+
+# 设置 PPPoE
+if [ -n "$PPPOE_USERNAME" ] && [ -n "$PPPOE_PASSWORD" ]; then
+  uci set network.wan.proto='pppoe'
+  uci set network.wan.username="$PPPOE_USERNAME"
+  uci set network.wan.password="$PPPOE_PASSWORD"
+  uci commit network
+fi
+
+# 设置 WiFi：纯 WPA3
+uci set wireless.@wifi-device[0].disabled='0'
+uci set wireless.@wifi-device[1].disabled='0' 2>/dev/null
+
+uci set wireless.@wifi-iface[0].disabled='0'
+uci set wireless.@wifi-iface[0].ssid="$WIFI_SSID"
+uci set wireless.@wifi-iface[0].encryption='sae'
+uci set wireless.@wifi-iface[0].key="$WIFI_PASSWORD"
+
+uci set wireless.@wifi-iface[1].disabled='0' 2>/dev/null
+uci set wireless.@wifi-iface[1].ssid="$WIFI_SSID" 2>/dev/null
+uci set wireless.@wifi-iface[1].encryption='sae' 2>/dev/null
+uci set wireless.@wifi-iface[1].key="$WIFI_PASSWORD" 2>/dev/null
+
+uci commit wireless
+
+exit 0
+EOF
+
+chmod +x files/etc/uci-defaults/99-custom-settings
